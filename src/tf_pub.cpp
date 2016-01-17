00bc2fbe854950e6d72cf0e810eb65cf5ac5bf7a
Jakob Auer <jakob.auer@student.tugraz.at>
2016-01-17 12:18:29 +0100

    added laser scanner

---
+++ src/tf_pub.cpp
@@ -0,0 +1,33 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_datatypes.h>
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "forbot_tf_publisher");
+  ros::NodeHandle n;
+
+  ros::Rate r(100);
+
+  tf::TransformBroadcaster broadcaster;
+
+  while(n.ok()){
+
+
+    broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.0)),
+        ros::Time::now(),"odom", "base_footprint"));
+
+    broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.0)),
+        ros::Time::now(),"base_footprint", "base_link"));
+
+    tf::Quaternion laser_quat = tf::createQuaternionFromRPY(3.1415 , 0.0, 0.0);
+    broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(laser_quat, tf::Vector3(0.42, 0.0, 0.1)),
+        ros::Time::now(),"base_link", "base_laser_link"));
+    r.sleep();
+  }
+}
